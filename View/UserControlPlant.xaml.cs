cc836530c8acf5d9a362213d055760f77f330e97
Gilford0306 <Gilford0306@gmail.com>
2023-07-12 09:02:09 +0300

    create base structure

---
+++ View/UserControlPlant.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookMedicinalPlants.View
+{
+    /// <summary>
+    /// Interaction logic for UserControlPlant.xaml
+    /// </summary>
+    public partial class UserControlPlant : UserControl
+    {
+        public static readonly DependencyProperty NameProdProperty = DependencyProperty.Register(nameof(MynameProd), typeof(string), typeof(UserControlPlant));
+        public string MynameProd
+        {
+            get { return (string)GetValue(NameProdProperty); }
+            set { SetValue(NameProdProperty, value); }
+        }
+        public static readonly DependencyProperty PriceProdProperty = DependencyProperty.Register(nameof(MypriceProd), typeof(string), typeof(UserControlPlant));
+        public string MypriceProd
+        {
+            get { return (string)GetValue(PriceProdProperty); }
+            set { SetValue(PriceProdProperty, value); }
+        }
+        public static readonly DependencyProperty PhotoProdProperty = DependencyProperty.Register(nameof(MyphotoProd), typeof(string), typeof(UserControlPlant));
+        public string MyphotoProd
+        {
+            get { return (string)GetValue(PhotoProdProperty); }
+            set { SetValue(PhotoProdProperty, value); }
+        }
+
+        public UserControlPlant()
+        {
+            InitializeComponent();
+            //prodName.Text = MynameProd;
+            //prodPrice.Text = MypriceProd;
+            //prodPhoto.Source =(ImageSource)new ImageSourceConverter().ConvertFromString(MyphotoProd);
+        }
+
+    }
+}
---
+++ ViewModel/MainVM.cs
@@ -0,0 +1,84 @@
+﻿using BookMedicinalPlants.Model;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace BookMedicinalPlants.ViewModel
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<Plant> Plants { get; set; }
+        private Plant selectedPlant;
+        private string selectedPlantName;
+
+        public Plant MySelectedPlant
+        {
+            get { return selectedPlant; }
+            set { selectedPlant = value; OnPropertyChanged("MySelectedPlant"); }
+        }
+
+
+        public string SelectedPlantName
+        {
+            get { return selectedPlantName; }
+            set { selectedPlantName = value; OnPropertyChanged("SelectedPlantName"); }
+        }
+
+
+
+        private RelayCommand searchCommand;
+
+        public RelayCommand SearchCommand
+        {
+            get
+            {
+                if (searchCommand == null)
+                    searchCommand = new RelayCommand(SearchPlant);
+
+                return searchCommand;
+            }
+        }
+        public void SearchPlant()
+        {
+            bool flag = false;
+            foreach (Plant plant in Plants)
+            {
+
+                if (plant.Name.ToLower() == selectedPlantName.ToLower())
+                {
+                    flag = true;
+                    MySelectedPlant = plant;
+                }
+            }
+            if (!flag)
+                MessageBox.Show("Растение не найдено");
+        }
+
+
+
+
+        public MainVM()
+        {
+            Plants = new ObservableCollection<Plant>();
+            //using (MyApplicationContext context = new MyApplicationContext())
+            //{
+            //    foreach (Drug drug in context.Drugs)
+            //    {
+            //        Drugs.Add(drug);
+            //    }
+            //}
+            MySelectedPlant = Plants[0];
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
