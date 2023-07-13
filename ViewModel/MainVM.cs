0bf2033c8d5811cfe798b96f8ca3c8c8bfa7156a
Gilford0306 <Gilford0306@gmail.com>
2023-07-13 13:16:24 +0300

    add addfuction save function  delfunction

--- ViewModel/MainVM.cs
+++ ViewModel/MainVM.cs
@@ -2,16 +2,19 @@
 using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace BookMedicinalPlants.ViewModel
 {
     internal class MainVM : INotifyPropertyChanged
     {
-
+        private int change = 0;
         public ObservableCollection<Plant> Plants { get; set; }
         public ObservableCollection<Plant> SelectedPlant { get; set; }
         private Plant mySelectedPlant;
@@ -20,7 +23,15 @@ namespace BookMedicinalPlants.ViewModel
         public Plant MySelectedPlant
         {
             get { return mySelectedPlant; }
-            set { mySelectedPlant = value; OnPropertyChanged("MySelectedPlant"); }
+            set
+            {
+                mySelectedPlant = value;
+                OnPropertyChanged("MySelectedPlant");
+                if (mySelectedPlant != null)
+                {
+                    change = mySelectedPlant.Id;
+                }
+            }
         }
 
 
@@ -42,7 +53,7 @@ namespace BookMedicinalPlants.ViewModel
                 {
                     searchCommand = new RelayCommand(SearchPlant);
                 }
-                    
+
 
                 return searchCommand;
             }
@@ -51,16 +62,21 @@ namespace BookMedicinalPlants.ViewModel
         {
             SelectedPlant.Clear();
             bool flag = false;
-            foreach (Plant plant in Plants)
+            if (selectedPlantName != null)
             {
-                if (plant.Name.ToLower().Contains(selectedPlantName.ToLower()) || plant.PublicName.ToLower().Contains(selectedPlantName.ToLower())|| plant.Description.ToLower().Contains(selectedPlantName.ToLower()) || plant.Region.ToLower().Contains(selectedPlantName.ToLower()) || plant.Plus.ToLower().Contains(selectedPlantName.ToLower()) || plant.Minus.ToLower().Contains(selectedPlantName.ToLower()))
+                foreach (Plant plant in Plants)
+                {
+                    if (plant.Name.ToLower().Contains(selectedPlantName.ToLower()) || plant.PublicName.ToLower().Contains(selectedPlantName.ToLower()) || plant.Description.ToLower().Contains(selectedPlantName.ToLower()) || plant.Region.ToLower().Contains(selectedPlantName.ToLower()) || plant.Plus.ToLower().Contains(selectedPlantName.ToLower()) || plant.Minus.ToLower().Contains(selectedPlantName.ToLower()))
                     {
-                    flag = true;
-                    SelectedPlant.Add(plant);
+                        flag = true;
+                        SelectedPlant.Add(plant);
+                    }
                 }
+                if (!flag)
+                    MessageBox.Show("Растение не найдено");
+
             }
-            if (!flag)
-                MessageBox.Show("Растение не найдено");
+
         }
 
         private RelayCommand saveCommand;
@@ -69,49 +85,125 @@ namespace BookMedicinalPlants.ViewModel
         {
             get
             {
-                if (searchCommand == null)
+                if (saveCommand == null)
                 {
-                    searchCommand = new RelayCommand(SavePlant);
+                    saveCommand = new RelayCommand(SavePlant);
                 }
 
 
-                return searchCommand;
+                return saveCommand;
             }
         }
         public void SavePlant()
         {
+            if (change > 0)
+            {
 
                 using (MyApplicationContext context = new MyApplicationContext())
                 {
-
-                    context.Plants.Add(new Plant() { Name = "test", PublicName = "test", Description = "test" });
+                    var oldData = context.Plants.FirstOrDefault(x => x.Id == change);
+                    oldData.Name = MySelectedPlant.Name;
+                    oldData.PublicName = MySelectedPlant.PublicName;
+                    oldData.Description = MySelectedPlant.Description;
+                    oldData.Plus = MySelectedPlant.Plus;
+                    oldData.Minus = MySelectedPlant.Minus;
+                    oldData.Icon = MySelectedPlant.Icon;
+                    context.Plants.Update(oldData);
                     context.SaveChanges();
-                    MessageBox.Show("Растение добавлено");
                 }
-            
+                MessageBox.Show("Изменения сохранены");
+            }
+
         }
 
 
 
+        private RelayCommand addCommand;
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                if (addCommand == null)
+                {
+                    addCommand = new RelayCommand(AddPlant);
+                }
+
+
+                return addCommand;
+            }
+        }
+        public void AddPlant()
+        {
+            using (MyApplicationContext context = new MyApplicationContext())
+            {
+
+
+
+                context.Plants.Add(new Plant() { Name = "Название", PublicName = "Название народное", Description = "Описание", Region = "Регион", Plus = "Положительные свойства", Minus = "Отрицательные  свойства", Icon = "URL фото" });
+                context.SaveChanges();
+                Plants.Clear();
+                foreach (Plant plant in context.Plants)
+                    {
+                        Plants.Add(plant);
+                    }
+
+            }
+
+        }
+
+
+        private RelayCommand delCommand;
+
+        public RelayCommand DelCommand
+        {
+            get
+            {
+                if (delCommand == null)
+                {
+                    delCommand = new RelayCommand(DelPlant);
+                }
+
+
+                return delCommand;
+            }
+        }
+        public void DelPlant()
+        {
+            if (change > 0)
+            {
+                using (MyApplicationContext context = new MyApplicationContext())
+                {
+
+                    context.Plants.Remove(context.Plants.FirstOrDefault(z => z.Id.Equals(change)));
+                    context.SaveChanges();
+                    MessageBox.Show("Растение удалено");
+                    Plants.Remove(Plants.FirstOrDefault(x => x.Id.Equals(change)));
+                    change = 0;
+                }
+
+            }
+        }
 
 
 
 
         public MainVM()
         {
-            Plants = new ObservableCollection<Plant>()
-            
-            //using (MyApplicationContext context = new MyApplicationContext())
-            //{
-            //    foreach (Drug drug in context.Drugs)
-            //    {
-            //        Drugs.Add(drug);
-            //    }
-            //}
+            Plants = new ObservableCollection<Plant>();
+
+
+            using (MyApplicationContext context = new MyApplicationContext())
             {
-                new Plant() { Id = 1, Name = "Name", PublicName = "PublicName", Description = "Description", Region = "region", Plus = "plus", Minus = "Minus", Icon = "https://upload.wikimedia.org/wikipedia/commons/e/e8/Logo_TST.png"},
-                new Plant() { Id = 2, Name = "Nme", PublicName = "PublicName", Description = "Description", Region = "region", Plus = "plus", Minus = "Minus", Icon = "https://upload.wikimedia.org/wikipedia/commons/e/e8/Logo_TST.png"}
-            };
+                foreach (Plant plant in context.Plants)
+                {
+                    Plants.Add(plant);
+                }
+            }
+            //{
+            //    new Plant() { Id = 1, Name = "Name", PublicName = "PublicName", Description = "Description", Region = "region", Plus = "plus", Minus = "Minus", Icon = "https://upload.wikimedia.org/wikipedia/commons/e/e8/Logo_TST.png"},
+            //    new Plant() { Id = 2, Name = "Nme", PublicName = "PublicName", Description = "Description", Region = "region", Plus = "plus", Minus = "Minus", Icon = "https://upload.wikimedia.org/wikipedia/commons/e/e8/Logo_TST.png"}
+            //};
             SelectedPlant = new ObservableCollection<Plant>();
 
         }
